8188ffdfcea430e53994c91069dc8975b946c947
M-Programer <90270404+M-Programer@users.noreply.github.com>
2022-06-20 20:28:50 +0200

    Add files via upload

---
+++ Programming Foundations Fundamentals/favourite_city.py
@@ -0,0 +1,6 @@
+def favorite_city(name):
+    print("One of my favourite cities is " + name)
+
+favorite_city("Biala")
+favorite_city("Port said")
+favorite_city("Alexandria")
---
+++ Programming Foundations Fundamentals/guess.py
@@ -0,0 +1,7 @@
+guess = input("What's my favorite food?")
+food = "frensh fries"
+if food == guess:
+    print("Yep! So amazing!")
+else:
+    print("That's not it!")
+print("Thanks for playing!")
